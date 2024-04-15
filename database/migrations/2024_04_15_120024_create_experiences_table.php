672c3763c04ca7bd456021a24603d1af739baedf
Eric Mai <eric.mai@ekami.ch>
2024-04-15 15:04:59 +0200

    created model and table with migrations

---
+++ database/migrations/2024_04_15_120024_create_experiences_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('site_name');
+            $table->string('title');
+            $table->string('activity');
+            $table->string('place');
+            $table->date('date');
+            $table->integer('distance');
+            $table->integer('priority');
+            $table->text('description');
+            $table->string('image');
+            $table->string('state');
+            $table->string('last_modif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experiences');
+    }
+};
